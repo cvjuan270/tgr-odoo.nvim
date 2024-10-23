9d05fd084d917f52fb117b769a457923642f4fd3
Juan Diego <juand@DESKTOP-7NGM793>
2024-10-23 15:52:56 -0500

    add init.lua basic

--- lua/init.lua
+++ lua/init.lua
@@ -1,98 +1,28 @@
--- Plugin: odoo.nvim
--- Archivo: lua/odoo/init.lua
-
+-- Configuración principal del plugin
 local M = {}
 
--- Configuración por defecto
-M.config = {
-	-- Rutas de búsqueda para módulos Odoo
-	odoo_paths = {
-		"~/odoo/addons",
-		"./addons",
-		"./custom_addons",
-	},
-	-- Extensiones de archivo para buscar
-	file_patterns = {
-		"*.py",
-		"*.xml",
-		"*/__manifest__.py",
-	},
-	-- Comandos personalizados
-	commands = {
-		enable = true,
-	},
-	-- Integración con LSP
-	lsp = {
-		enable = true,
-		python_path = vim.fn.exepath("python3"),
-	},
-}
-
--- Función de configuración
+-- Función para inicializar el plugin
 function M.setup(opts)
-	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+	-- Configuraciones predeterminadas
+	M.options = opts or {}
 
-	-- Registrar comandos de Neovim
-	if M.config.commands.enable then
-		M.create_commands()
-	end
-
-	-- Configurar autocompletado y LSP
-	if M.config.lsp.enable then
-		M.setup_lsp()
-	end
-
-	-- Configurar keymaps
-	M.setup_keymaps()
+	-- Aquí cargaremos nuestras funciones
+	M.load_navigation()
+	M.setup_autocomplete()
+	M.setup_orm_management()
 end
 
--- Función para registrar comandos
-function M.create_commands()
-	vim.api.nvim_create_user_command("OdooGotoModel", function(opts)
-		require("odoo.navigation").goto_model(opts.args)
-	end, {
-		nargs = 1,
-		complete = function()
-			return require("odoo.utils").list_models()
-		end,
-	})
-
-	vim.api.nvim_create_user_command("OdooGotoView", function(opts)
-		require("odoo.navigation").goto_view(opts.args)
-	end, {
-		nargs = 1,
-		complete = function()
-			return require("odoo.utils").list_views()
-		end,
-	})
+-- Funciones del plugin
+function M.load_navigation()
+	print("Navegación para Odoo cargada")
 end
 
--- Función para configurar LSP
-function M.setup_lsp()
-	local lspconfig = require("lspconfig")
-
-	-- Configurar el servidor LSP de Python con conocimiento específico de Odoo
-	lspconfig.pyright.setup({
-		settings = {
-			python = {
-				analysis = {
-					extraPaths = M.config.odoo_paths,
-				},
-			},
-		},
-	})
+function M.setup_autocomplete()
+	print("Autocompletado específico de Odoo activado")
 end
 
--- Función para configurar keymaps
-function M.setup_keymaps()
-	local keymap = vim.keymap.set
-	local opts = { noremap = true, silent = true }
-
-	-- Navegación rápida
-	keymap("n", "<Leader>om", ":OdooGotoModel ", opts)
-	keymap("n", "<Leader>ov", ":OdooGotoView ", opts)
-
-	-- Más keymaps específicos de Odoo...
+function M.setup_orm_management()
+	print("Gestión del ORM de Odoo configurada")
 end
 
 return M
